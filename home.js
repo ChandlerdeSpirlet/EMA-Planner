bfbd69facd370923be566ed1d166c2fa3f6142de
Chandler de Spirlet <chandler.despirlet@icloud.com>
2020-08-20 11:22:53 -0600

    Working on class lookup tool

--- home.js
+++ home.js
@@ -374,6 +374,21 @@ router.get('/class_selector', (req, res) => {
         })
 });
 
+router.get('/class_selector_force/(:month)/(:day)', (req, res) => {
+    const date_conversion = req.params.month + ' ' + req.params.day'
+    var query = "select class_id, to_char(starts_at, 'Month') as class_month, to_char(starts_at, 'DD') as class_day, to_char(starts_at, 'HH:MI') as class_time, to_char(ends_at, 'HH:MI') as end_time, level from classes where to_char(starts_at, 'Month DD') = to_char(to_date('$1', 'Month DD'), 'Month DD') order by starts_at;";
+    db.any(query, [date_conversion])
+        .then(function(rows){
+            res.render('class_selector', {
+                data: rows
+            });
+        })
+        .catch(function(err){
+            console.log('error in getting classes ' + err);
+            res.redirect('home');
+        })
+});
+
 router.get('/class_remove/(:barcode)/(:class_id)', (req, res) => {
     const remove_query = 'delete from student_classes where class_id = $1 and barcode = $2;';
     db.any(remove_query, [req.params.class_id, req.params.barcode])
@@ -466,6 +481,21 @@ router.get('/payment_resolved/(:id)', (req, res) => {
             res.redirect('/');
         })
 });
+
+router.get('/class_lookup', (req, res) => {
+    res.render('class_lookup', {
+
+    })
+});
+
+router.post('/class_lookup', (req, res) => {
+    var item = {
+        month: req.sanitize('month_select').trim(),
+        day: req.sanitize('day_select').trim()
+    }
+    const redir_link = 'class_selector_force/' + item.month + '/' + item.day;
+    res.redirect(redir_link);
+})
 
 app.post('/webhook', (request, response) => {
     let event;
