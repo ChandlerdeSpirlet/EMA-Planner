169a0195b77c9d87b345eefd8c85b51ca8dfafe2
Chandler de Spirlet <chandler.despirlet@icloud.com>
2021-04-30 22:05:00 -0600

    Update home.js

--- home.js
+++ home.js
@@ -65,6 +65,7 @@ app.get('/', (req, res) => {
   if (req.headers['x-forwarded-proto'] != 'https') {
     res.redirect('https://ema-planner.herokuapp.com/')
   } else {
+    var date = new Date();
     var d = date.toLocaleString("en-US", {timeZone: "America/Denver"});
     var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     month = months[d.getMonth()];
