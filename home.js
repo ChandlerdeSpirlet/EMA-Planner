f04084be6ad43dbddbb3c6ae889729879bda4866
Chandler de Spirlet <chandler.despirlet@icloud.com>
2021-06-02 19:21:48 -0600

    Version 6.0.0
    Working on PaySimple Integration

--- home.js
+++ home.js
@@ -8,6 +8,21 @@ const ics = require('ics')
 const { writeFileSync } = require('fs')
 const { readFileSync } = require('fs')
 const nodemailer = require("nodemailer");
+'use strict';
+const request = require('request');
+const crypto = require('crypto');
+
+const settings = {
+  apiv4url: https://sandbox-api.paysimple.com/v4,
+  username: 'APIUser99999',
+  apikey: 'blah'
+}
+
+function getAuthHeader(){
+  let time = (new Date()).toISOString();
+  let hash = crypto.createHmac('SHA256', settings.apikey).update(time).digest('base64');
+  return 'PSSERVER ' + "accessid=" + settings.username + "; timestamp=" + time + "; signature=" + hash;
+}
 
 const app = express()
 app.use(flash());
@@ -60,6 +75,27 @@ function convertToMoney(amount) {
   })
   return formatter.format(amount / 100)
 }
+
+app.get('/token', (req, res) => {
+  let options = {
+    method: "POST",
+    uri: settings.apiv4url + '/checkouttoken',
+    headers: {
+      Authorization: getAuthHeader()
+    },
+    body: {},
+    json: true,
+  };
+
+  request(options, function(error, response, body) {
+    if (!error && response && res.statusCode < 300) {
+      res.json(body.Response);
+      return;
+    }
+    res.status((response && response.statusCode) || 500).send(error);
+  });
+});
+
 
 
 app.get('/', (req, res) => {
